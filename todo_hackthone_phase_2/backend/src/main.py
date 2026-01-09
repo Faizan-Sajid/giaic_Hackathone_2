cb4fa95f41811b5eeff1dc933fda75b7f99eee8f
faizan sajid <165155018+Faizan-Sajid@users.noreply.github.com>
2026-01-09 18:02:22 +0500

    Fix CORS for deployment

--- todo_hackthone_phase_2/backend/src/main.py
+++ todo_hackthone_phase_2/backend/src/main.py
@@ -43,7 +43,7 @@ app = FastAPI(
 # Explicitly configure for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # ONLY allow this origin
+    allow_origins=["https://giaic-hackathone-2.vercel.app"],  # ONLY allow this origin
     allow_credentials=True,  # Required for cookies - set BEFORE other middleware
     allow_methods=["*"],
     allow_headers=["*"],
